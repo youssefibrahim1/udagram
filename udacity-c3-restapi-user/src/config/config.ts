69550460e25b600c081089dfc70070f750ca6ae4
Youssef Ibrahim <youssefibrahim@Youssefs-MacBook-Pro.local>
2020-05-14 08:27:27 -0400

    app-working

--- udacity-c3-restapi-user/src/config/config.ts
+++ udacity-c3-restapi-user/src/config/config.ts
@@ -1,24 +1,24 @@
 export const config = {
-  "dev": {
-    "username": process.env.POSTGRESS_USERNAME,
-    "password": process.env.POSTGRESS_PASSWORD,
-    "database": process.env.POSTGRESS_DB,
-    "host": process.env.POSTGRESS_HOST,
-    "dialect": "postgres",
-    "aws_reigion": process.env.AWS_REGION,
-    "aws_profile": process.env.AWS_PROFILE,
-    "aws_media_bucket": process.env.AWS_BUCKET,
-    "url": process.env.URL
+  dev: {
+    username: process.env.AWS_USERNAME,
+    password: process.env.AWS_PASSWORD,
+    database: process.env.AWS_DATABASE,
+    host: process.env.AWS_HOST,
+    dialect: "postgres",
+    aws_region: process.env.AWS_REGION,
+    aws_profile: process.env.AWS_PROFILE,
+    aws_media_bucket: process.env.AWS_BUCKET,
+    url: "http://localhost:8100",
   },
-  "prod": {
-    "username": "",
-    "password": "",
-    "database": "udagram_prod",
-    "host": "",
-    "dialect": "postgres"
+  prod: {
+    username: "",
+    password: "",
+    database: "udagram_prod",
+    host: "",
+    dialect: "postgres",
+    url: process.env.AWS_URL,
   },
-  "jwt": {
-    "secret": process.env.JWT_SECRET
-  }
-
-}
+  jwt: {
+    secret: process.env.JWT_SECRET,
+  },
+};
